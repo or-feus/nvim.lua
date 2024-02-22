97dc583d41bb94fa4a13cee6e7797912f9e7b211
or-feus <barca105@nate.com>
2024-02-22 10:51:17 +0900

    Add set tabstop 2 for tf

--- lua/plugins/lsp/terraform.lua
+++ lua/plugins/lsp/terraform.lua
@@ -5,3 +5,12 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
         vim.lsp.buf.format()
     end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "terraform",
+    callback = function()
+        vim.opt_local.shiftwidth = 2
+        vim.opt_local.tabstop = 2
+        vim.opt_local.expandtab = true
+    end,
+})
